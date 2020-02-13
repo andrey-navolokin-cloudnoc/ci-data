ef6709a88c876baadba23f7cbebcb37f2dd0206c
Andrey Navolokin <43013998+andrey-navolokin-cloudnoc@users.noreply.github.com>
2020-02-14 00:33:29 +0700

    Update and rename user-data.txt to user-data.sh

---
+++ user-data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "fuck" > /root/fuck
+echo "fist" > /root/first
+echo "{{ v1.region }}" > /root/region
+echo "{{ ds.meta-data.ami-id"" > /root/ami_id
+echo "{{ default_username }} > /root/username
